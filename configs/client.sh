ae4f5c4c19de8367f3810083bab8d5904d00f5af
Roman Dodin <dodin.roman@gmail.com>
2024-01-16 00:38:45 +0200

    remove explicit null and fix qinq

--- configs/client.sh
+++ configs/client.sh
@@ -20,17 +20,14 @@ ip addr add 10.1.2.${ID}/30 dev eth1.11
 ip link set dev eth1.11 address aa:c1:ab:00:02:0${ID}
 ip link set dev eth1.11 up
 
-# qinq outer tag 13, inner tag 12
-ip link add name eth1.12 link eth1 type vlan proto 802.1ad id 12 # inner tag
-ip link add name eth1.12.13 link eth1.12 type vlan proto 802.1q id 13 # outer tag
+# qinq outer tag 12, inner tag 13
+ip link add name eth1.12 link eth1 type vlan proto 802.1q id 12 # outer
+ip link add name eth1.12.13 link eth1.12 type vlan proto 802.1q id 13 # inner tag
 ip addr add 10.1.3.${ID}/30 dev eth1.12.13
 ip link set dev eth1.12 address aa:c1:ab:03:00:0${ID}
 ip link set dev eth1.12 up
 ip link set dev eth1.12.13 address aa:c1:ab:03:03:0${ID}
 ip link set dev eth1.12.13 up
 
-# null tag
-ip link add name eth1.0 link eth1 type vlan id 0
-ip addr add 10.1.4.${ID}/30 dev eth1.0
-ip link set dev eth1.0 address aa:c1:ab:00:04:0${ID}
-ip link set dev eth1.0 up
+# explicit null tag case is not covered
+# since it is a quirky case that varies from vendor to vendor
--- ping.sh
+++ ping.sh
@@ -27,15 +27,10 @@ if [ "$CASE" == "double-tag" ]; then
     ip netns exec clab-vlan-client1 ping -c 1 10.1.3.2
 fi
 
-if [ "$CASE" == "0-tag" ]; then
-    ip netns exec clab-vlan-client1 ping -c 1 10.1.4.2
-fi
-
 if [ "$CASE" == "all" ]; then
     # ping all cases and print the results that pass and not pass
     ping_test 10.1.0.2 "no tag"
     ping_test 10.1.1.2 "single tag VID: 10"
     ping_test 10.1.2.2 "single tag VID: 11"
-    ping_test 10.1.3.2 "double tag outer VID: 13, inner VID: 12"
-    ping_test 10.1.4.2 "null tag"
+    ping_test 10.1.3.2 "double tag outer VID: 12, inner VID: 13"
 fi

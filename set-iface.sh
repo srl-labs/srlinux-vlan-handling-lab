def55f7a408108aa76a9b0455fe02d84b0703249
Roman Dodin <dodin.roman@gmail.com>
2024-01-15 20:54:05 +0200

    use gnmic config file

--- set-iface.sh
+++ set-iface.sh
@@ -5,11 +5,9 @@ declare -a srl_nodes=("clab-vlan-srl1" "clab-vlan-srl1")
 pwd=$(pwd)
 
 # Save the command in a variable
-gnmic="docker run --network clab --rm -t -v ${pwd}/configs:/configs ghcr.io/openconfig/gnmic:0.34.3 -u admin -p NokiaSrl1! -e json_ietf --skip-verify -a"
+gnmic="docker run --network clab --rm -t -v ${pwd}/configs:/configs ghcr.io/openconfig/gnmic:0.34.3 --config /configs/nodes.yml"
 
 MODE=$1
-
-# if mode == "singele-tag"
 
 if [ "$MODE" == "single-tag" ]; then
     REPLACE_FILE=configs/single-tag-10.yml
@@ -23,6 +21,5 @@ if [ "$MODE" == "untagged" ]; then
     REPLACE_FILE=configs/untagged.yml
 fi
 
-for srl in "${srl_nodes[@]}"; do
-    $gnmic ${srl} set --replace-path /interface[name=ethernet-1/1] --replace-file ${REPLACE_FILE}
-done
+
+$gnmic ${srl} set --replace-path /interface[name=ethernet-1/1] --replace-file ${REPLACE_FILE}

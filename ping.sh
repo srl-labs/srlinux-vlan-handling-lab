e7b0a6a19fcb2c6d24f221daa009ad119e684b14
Roman Dodin <dodin.roman@gmail.com>
2024-05-20 12:48:34 +0300

    use client1 netns when running in c9s

--- ping.sh
+++ ping.sh
@@ -2,8 +2,14 @@
 
 CASE=$1
 
+PING_NS=clab-vlan-client1
+
+if [ "$APP_NAME" == "clabernetes" ]; then
+    PING_NS="client1"
+fi
+
 ping_test() {
-    ip netns exec clab-vlan-client1 ping -c 1 -w 1 $1 > /dev/null
+    ip netns exec ${PING_NS} ping -c 1 -w 1 $1 > /dev/null
     if [ $? -eq 0 ]; then
         echo -e "+ \e[32mPing to $1 ($2) was successful.\e[0m"
     else
@@ -12,19 +18,19 @@ ping_test() {
 }
 
 if [ "$CASE" == "no-tag" ]; then
-    ip netns exec clab-vlan-client1 ping -c 1 10.1.0.2
+    ip netns exec ${PING_NS} ping -c 1 10.1.0.2
 fi
 
 if [ "$CASE" == "single-tag-10" ]; then
-    ip netns exec clab-vlan-client1 ping -c 1 10.1.1.2
+    ip netns exec ${PING_NS} ping -c 1 10.1.1.2
 fi
 
 if [ "$CASE" == "single-tag-11" ]; then
-    ip netns exec clab-vlan-client1 ping -c 1 10.1.2.2
+    ip netns exec ${PING_NS} ping -c 1 10.1.2.2
 fi
 
 if [ "$CASE" == "double-tag" ]; then
-    ip netns exec clab-vlan-client1 ping -c 1 10.1.3.2
+    ip netns exec ${PING_NS} ping -c 1 10.1.3.2
 fi
 
 if [ "$CASE" == "all" ]; then
